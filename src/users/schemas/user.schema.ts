1e71f2b2f4b2de69722e25a7732f2e10bffd3fef
unknown <huy.do02062004@gmail.com>
2025-11-24 19:38:49 +0700

    21. Create Schema (Model)

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,35 @@
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    Password: string;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    phone: string;
+
+    @Prop()
+    age: number;
+
+    @Prop()
+    address: string;
+
+    @Prop()
+    createdAt: Date;
+
+    @Prop()
+    updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
