070afce59d4b1f7c0554c4cc8e0e08df7e34f6b3
unknown <huy.do02062004@gmail.com>
2025-12-05 20:21:25 +0700

    05/12/2025

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -2,14 +2,22 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { User } from 'src/decorator/customize';
+import { IUsers } from 'src/users/users.interface';
 
 @Controller('companies')
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) { }
 
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto) {
-    return this.companiesService.create(createCompanyDto);
+  create(@Body() createCompanyDto: CreateCompanyDto, @User() user: IUsers) {
+    console.log("user infor: ", user)
+    return this.companiesService.create(createCompanyDto, user);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto, @User() user: IUsers) {
+    return this.companiesService.update(id, updateCompanyDto, user);
   }
 
   @Get()
@@ -22,13 +30,10 @@ export class CompaniesController {
     return this.companiesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
-    return this.companiesService.update(+id, updateCompanyDto);
-  }
+
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.companiesService.remove(+id);
+  remove(@Param('id') id: string, @User() user: IUsers) {
+    return this.companiesService.remove(id, user);
   }
 }
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,19 +4,38 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUsers } from 'src/users/users.interface';
 
 @Injectable()
 export class CompaniesService {
 
   constructor(@InjectModel(Company.name) private companyModel: SoftDeleteModel<CompanyDocument>) { }
 
-  async create(createCompanyDto: CreateCompanyDto) {
+  async create(createCompanyDto: CreateCompanyDto, user: IUsers) {
     let company = await this.companyModel.create({
       name: createCompanyDto.name,
       address: createCompanyDto.address,
-      description: createCompanyDto.description
+      description: createCompanyDto.description,
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
     })
     return company
+  }
+
+  async update(id: string, updateCompanyDto: UpdateCompanyDto, user: IUsers) {
+    let companyUpdate = await this.companyModel.updateOne({ _id: id }, {
+      name: updateCompanyDto.name,
+      address: updateCompanyDto.address,
+      description: updateCompanyDto.description,
+      updatedBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
+
+    return companyUpdate
   }
 
   findAll() {
@@ -27,11 +46,19 @@ export class CompaniesService {
     return `This action returns a #${id} company`;
   }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+
+  async remove(id: string, user: IUsers) {
+    await this.companyModel.updateOne({ _id: id }, {
+      deletedBy: {
+        _id: user._id,
+        email: user.email
+      }
+    })
+
+    return await this.companyModel.softDelete({
+      _id: id
+    })
+
   }
 }
